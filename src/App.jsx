0074ab804796a27bb1ef6584a83c57b0fa0c9221
josueW913 <josuew913@gmail.com>
2023-07-28 09:42:44 -0400

    lab done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import Navbar from "./components/Navbar";
+import CountryDetailsPage from "./pages/CountryDetailsPage";
+
 
 function App() {
+
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+
+    axios.get('https://ih-countries-api.herokuapp.com/countries')
+      .then((response) => {
+        console.log("Countries:", response.data)
+        setCountries(response.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+
+  }, []);
+
   return (
     <div className="App">
-      <h1>LAB | React WikiCountries</h1>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage countries={countries} />} />
+        <Route path="/:countryId" element={<CountryDetailsPage countries={countries} />} />
+      </Routes>
+
     </div>
   );
 }
